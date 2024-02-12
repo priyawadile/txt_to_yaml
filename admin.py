6607b906928bdf9f5a7c4951eb4320077e3a0f2b
priyawadile <89268570+priyawadile@users.noreply.github.com>
2024-02-13 01:48:30 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from txtyaml.models import TextFile, UploadFileForm
-
-
-# Register your models here.
-admin.site.register(TextFile)
-admin.site.register(UploadFileForm)
